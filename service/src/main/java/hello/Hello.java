e2019ca2d2153d2abfde5a558221ec2c26ccf9d5
LEONARDO DINIZ CORREIA DOS SANTOS <0040481422033@fatec.br>
2018-03-05 17:10:29 -0300

    adicionando ao projeto o servico de back-end

---
+++ service/src/main/java/hello/Hello.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 0040481422033
+ */
+package hello;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration
+public class Hello {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Hello.class, args);
+    }
+}
