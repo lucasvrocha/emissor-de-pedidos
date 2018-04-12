1c460a579d46a3da7076f9c074bb8e5280f4b84f
Lucas Rocha <lvr132321@gmail.com>
2018-04-12 16:38:22 -0300

    Close #63 - Foi melhorado o modulo de backend/backend

--- site/src/app/_mock/backend/backend.provider.ts
+++ site/src/app/_mock/backend/backend.provider.ts
@@ -1,48 +1,56 @@
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpResponse, HttpErrorResponse, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
+@Injectable()
+export class NoopInterceptor implements HttpInterceptor {
+	intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+		return next.handle(req);
+	}
+}
+
+// -------------------------
+
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { environment as env } from '../../../environments/environment';
 import { BackendInterceptor } from './interceptor/backend.interceptor';
 import { LoggerInterceptor } from './interceptor/logger.interceptor';
 import { DelayInterceptor } from './interceptor/delay.interceptor';
 import { ProxyInterceptor } from './interceptor/proxy.interceptor';
 
-const ProxyProvider = env.proxy && env.proxy.length>0 ? {
+export const NoopeProvider = {
+	provide: HTTP_INTERCEPTORS,
+	useClass: NoopInterceptor,	
+	multi: true
+}
+
+export const ProxyProvider = env.proxy && env.proxy.length > 0 ? {
 	provide: HTTP_INTERCEPTORS,
 	useClass: ProxyInterceptor,
 	multi: true
-} : undefined;
-	
-const LoggerProvider = env.logger && env.logger.init ? {
+} : NoopeProvider;
+
+export const LoggerProvider = env.logger && env.logger.init ? {
 	provide: HTTP_INTERCEPTORS,
 	useClass: LoggerInterceptor,
 	multi: true
-} : undefined;
+} : NoopeProvider;
 
-const DelayProvider = env.delay && env.delay > 0 ? {
+export const DelayProvider = env.delay && env.delay > 0 ? {
 	provide: HTTP_INTERCEPTORS,
 	useClass: DelayInterceptor,
 	multi: true
-} : undefined;
+} : NoopeProvider;
 
-const ResponseProvider = {
+export const ResponseProvider = {
 	provide: HTTP_INTERCEPTORS,
 	useClass: BackendInterceptor,
 	multi: true
 }
 
-export function factory() {
-	if (!env.mock)
-		return [];
-
-	let p = [];
-	if (ProxyProvider) p.push(ProxyProvider);
-	if (LoggerProvider) p.push(LoggerProvider);
-	if (DelayProvider) p.push(DelayProvider);
-	if (ResponseProvider) p.push(ResponseProvider);
-	
-	return p;
-};
-
-export let BackendProvider = [].concat(factory())
-
-
+export let BackendProvider = [
+	ProxyProvider, 
+	LoggerProvider,
+	DelayProvider,
+	ResponseProvider
+]
