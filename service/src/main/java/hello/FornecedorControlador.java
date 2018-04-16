5e09a8bde2699b08a4f1f3b4e74caef27af5bef0
LEONARDO DINIZ CORREIA DOS SANTOS <0040481422033@fatec.br>
2018-04-16 15:06:50 -0300

    sdd

--- service/src/main/java/hello/FornecedorControlador.java
+++ service/src/main/java/hello/FornecedorControlador.java
@@ -6,42 +6,44 @@ package hello;
   import org.springframework.web.bind.annotation.PathVariable;
   import org.springframework.web.bind.annotation.RequestMapping;
   import org.springframework.web.bind.annotation.RequestMethod;
-   
+  import org.springframework.boot.autoconfigure.EnableAutoConfiguration; 
 
    
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
    
 @Controller
 @RequestMapping("/fornecedor")
 public class FornecedorControlador {
+    @Autowired
     private FornecedorInterfaceRepositorio FornecedorRepositorio;
     
-        @Autowired
-        public FornecedorControlador( FornecedorInterfaceRepositorio fornecedorRepositorio) {
+        /*public FornecedorControlador( FornecedorInterfaceRepositorio fornecedorRepositorio) {
               this.FornecedorRepositorio = fornecedorRepositorio;
         }
-   
+   */
         // RETORNA UMA LISTA CONTENDO TODOS OS REGISTROS 
+        @ResponseBody
         @RequestMapping(value = "/all", method = RequestMethod.GET)
         public List<Fornecedor> listaFornecedor() {
               return (List<Fornecedor>) FornecedorRepositorio.findAllFornecedor();
         }
         
         // RETORNA FORNECEDOR DE DETERMINADO CNPJ
-//        @RequestMapping(value = "/{CNPJFornecedor}", method = RequestMethod.GET)
-//        public String listaFornecedor(@PathVariable("CNPJ") String CNPJFornecedor, Model model) {
-//              List<Fornecedor> listaFornecedor = FornecedorRepositorio.findByCNPJFornecedor(CNPJFornecedor);
-//              if (listaFornecedor != null) {
-//                    model.addAttribute("Fornecedores", listaFornecedor);
-//              }
-//              return "listaLivros";
-//        };
+        @ResponseBody
+        @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+        public Fornecedor fornecedor(@PathVariable("id") String id) {
+              Fornecedor fornecedor = FornecedorRepositorio.findByIDFornecedor(Integer.parseInt(id));
+              
+              return fornecedor;
+        };
    
         // ADICIONA UM NOVO FORNECEDOR
-        @RequestMapping(value = "/{CNPJFornecedor}", method = RequestMethod.POST)
+        @ResponseBody
+        @RequestMapping( method = RequestMethod.POST)
         public String adicionaFornecedorCNPJ(@PathVariable("CNPJFornecedor") String CNPJFornecedor, Fornecedor fornecedor) {
               fornecedor.setCNPJFornecedor(CNPJFornecedor);
               FornecedorRepositorio.addFornecedor(fornecedor);
@@ -49,6 +51,7 @@ public class FornecedorControlador {
         }
         
         // ATUALIZA UM FORNECEDOR
+        @ResponseBody
         @RequestMapping(value = "/{CNPJFornecedor}", method = RequestMethod.PUT)
 	public ResponseEntity<Fornecedor> updateArticle(@RequestBody Fornecedor fornecedor) {
 		FornecedorRepositorio.updateFornecedor(fornecedor);
@@ -56,9 +59,17 @@ public class FornecedorControlador {
 	}
         
         // DELETE UM NOVO FORNECEDOR
+        @ResponseBody
         @RequestMapping(value = "/{CNPJFornecedor}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> deleteArticle(@PathVariable("IDFornecedor") Integer idfornecedor) {
 		FornecedorRepositorio.deleteFornecedor(idfornecedor);
 		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
-	}	
+	}
+    
+        /*@ResponseBody
+        @RequestMapping(value = "/all", method = RequestMethod.GET)
+        public String listaFornecedor() {
+              return "Foi";
+        }*/
+    
 }
